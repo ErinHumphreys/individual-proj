f8042e4f7b5fcfe7c51340febb412acbeca8bb65
ErinHumphreys <eachumphreys@gmail.com>
2020-10-29 19:28:27 +0200

    adding tic-tac-toe game

---
+++ Tic-Tac-Toe-SC/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="title">
+        <span className="red"> SAMURAI CHAMPLOO </span>
+        <span className="red"> TIC </span>
+        <span className="orange"> TAC </span>
+        <span className="blue"> TOE </span>
+      </div>
+    </div>
+  );
+};
